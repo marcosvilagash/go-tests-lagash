7ba8cff6ad475fcad6b3c5d3313953f28684191f
Marcos Villanueva <marcosvi@lagash.com>
2019-12-16 11:42:57 -0300

    Add templates

--- api-rest-go/main.go
+++ api-rest-go/main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8000", router))
+	templates := populateTemplates()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		requestedFile := r.URL.Path[1:]
+		t := templates.Lookup(requestedFile + ".html")
+		if t != nil {
+			err := t.Execute(w, nil)
+			if err != nil {
+				log.Println(err)
+			}
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	})
+	http.ListenAndServe(":8080", nil)
 }
 
-// handler echoes the Path component of the request URL r.
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+func populateTemplates() *template.Template {
+	result := template.New("templates")
+	const basePath = "templates"
+	template.Must(result.ParseGlob(basePath + "/*.html"))
+	return result
 }
