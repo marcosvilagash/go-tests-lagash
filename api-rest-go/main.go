73f5a87437c7d7cc3d18c262da9d69123898ecb4
Marcos Villanueva <marcosvi@lagash.com>
2019-12-16 12:51:22 -0300

    Add parameter mariano

--- api-rest-go/main.go
+++ api-rest-go/main.go
@@ -6,13 +6,22 @@ import (
 	"net/http"
 )
 
+// Context is the page context
+type Context struct {
+	Fecha        string `json:"date"`
+	DolarOficial string `json:"officialdolar"`
+	DolarBlue    string `json:"bluedollar"`
+	Variacion    string `json:"variation"`
+}
+
 func main() {
 	templates := populateTemplates()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		requestedFile := r.URL.Path[1:]
 		t := templates.Lookup(requestedFile + ".html")
 		if t != nil {
-			err := t.Execute(w, nil)
+			ctx := Context{Fecha: "mandale el parametro"}
+			err := t.Execute(w, ctx)
 			if err != nil {
 				log.Println(err)
 			}
